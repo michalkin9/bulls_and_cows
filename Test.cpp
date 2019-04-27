a32effc34053d2035b2e843b558a5102723d1e25
adiweisberg <adi.weisberg5@gmail.com>
2019-04-27 15:04:10 +0300

    Add Tests #2

--- Test.cpp
+++ Test.cpp
@@ -26,12 +26,13 @@ int main() {
 	if (signal == 0) {
 
 		// BASIC TESTS - DO NOT CHANGE
-		ConstantChooser c1234{"1234"}, c12345{"12345"}, c9999{"9999"};
-		ConstantGuesser g1234{"1234"}, g12345{"12345"}, g9999{"9999"};
+		ConstantChooser c1234{"1234"}, c12345{"12345"}, c9999{"9999"}, c22{"22"}, c123{"123"}, c123456{"123456"};
+		ConstantGuesser g1234{"1234"}, g12345{"12345"}, g9999{"9999"}, g22{"22"}, g45{"45"}, g123456{"123456"}, g123{"123"};
 
 		testcase.setname("Calculate bull and pgia")
 		.CHECK_OUTPUT(calculateBullAndPgia("1234","1234"), "4,0")      // 4 bull, 0 pgia
 		.CHECK_OUTPUT(calculateBullAndPgia("1234","4321"), "0,4")      // 0 bull, 4 pgia
+		
 		/////////////////////////////// - Our Tests - ///////////////////////////////
 		.CHECK_OUTPUT(calculateBullAndPgia("1234","1324"), "2,2")      // 2 bull, 2 pgia
 		.CHECK_OUTPUT(calculateBullAndPgia("0000","1111"), "0,0")	   // 0 bull, 0 pgia
@@ -46,6 +47,10 @@ int main() {
 		.CHECK_EQUAL(play(c1234, g9999, 4, 100), 101)    // guesser loses by running out of turns 
 		.CHECK_EQUAL(play(c1234, g12345, 4, 100), 101)   // guesser loses technically by making an illegal guess (too long).
 		.CHECK_EQUAL(play(c12345, g1234, 4, 100), 0)     // chooser loses technically by choosing an illegal number (too long).
+		.CHECK_EQUAL(play(c22, g22, 2, 100), 1)			 // guesser wins in one turn.
+		.CHECK_EQUAL(play(c22, g45, 2, 100), 101)		 // guesser loses by running out of turns 
+		.CHECK_EQUAL(play(c123456, g12345, 5, 100), 0)   // chooser loses technically by choosing an illegal number (too long).
+		.CHECK_EQUAL(play(c1234,g123456, 4, 100), 101)	 // chooser loses technically by choosing an illegal number (too long).
 		;
 
 		testcase.setname("Play with smart guesser");
@@ -54,6 +59,9 @@ int main() {
 		for (uint i=0; i<100; ++i) {
 			testcase.CHECK_EQUAL(play(randy, smarty, 4, 100)<=10, true);  // smarty should always win in at most 10 turns!
 		}
+		
+		/////////////////////////////// - Our Tests - ///////////////////////////////
+		
 		for (uint i=0; i<100; ++i) { 
 			testcase.CHECK_EQUAL(play(c1234, smarty, 4, 100)<=10, true);  // Dummy chooser vs. smart guesser
 		}
@@ -64,6 +72,27 @@ int main() {
 			testcase.CHECK_EQUAL(play(c12345, smarty, 3, 100), 0);  // chooser loses technically by choosing an illegal number (too long).
 		}
 		
+		
+		testcase.setname("Check learn")
+		c1234.learn("1,2")
+		CHECK_EQUAL(c1234.bull, 1)
+		CHECK_EQUAL(c1234.pgia, 2)
+		c22.learn("2,0")
+		CHECK_EQUAL(c22.bull, 2)
+		CHECK_EQUAL(c22.pgia, 0)
+		g123456.learn("3,3")
+		CHECK_EQUAL(g123456.bull, 3)
+		CHECK_EQUAL(g123456.pgia, 3)
+		
+		
+		testcase.setname("Check learn with invalid input")
+		//"Caught exception! - the sum of bull+pgia is not equal to the length of the player choice"
+		CHECK_THROWS(c1234.learn("3,3"))
+		CHECK_THROWS(c22.learn("0,4"))
+		CHECK_THROWS(g12345.learn("2,5"))
+		CHECK_THROWS(g45.learn("2,2"))
+		CHECK_THROWS(g45.learn("3,0"))
+
 		
 		
 		
