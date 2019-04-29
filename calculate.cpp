984b51189f380543214e737a3cef5f29c8680f39
adiweisberg <adi.weisberg5@gmail.com>
2019-04-29 18:05:36 +0300

    Update calculate.cpp

--- calculate.cpp
+++ calculate.cpp
@@ -5,7 +5,24 @@
 
 using std::string;
 
-string bullpgia::calculateBullAndPgia(string chooser,string guesser) {
+string bullpgia::calculateBullAndPgia(string chooser, string guesser) {
+    int bull = 0, pgia = 0;
     string ans;
+    int size = chooser.length();
+    int arr[size];
+    if(size != guesser.length()){
+        throw "The guess's length is not equal to the choice's length";
+    }
+    for(int i=0; i<size; i++){
+        if(chooser[i] == guesser[i]) bull++;
+        else{
+            size_t found = chooser.find(guesser[i]); 
+            if(found<size && arr[found] != -1){
+                pgia++;
+                arr[found] = -1;
+            }
+    }
+    ans = bull +","+ pgia;
     return ans;
+}
 }
