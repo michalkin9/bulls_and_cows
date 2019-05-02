744babe097f6fe265f145c931a2913e94ddba463
Michal Yelkin <44944939+michalkin9@users.noreply.github.com>
2019-05-02 17:28:02 +0300

    more functions

--- SmartGuesser.cpp
+++ SmartGuesser.cpp
@@ -1,7 +1,8 @@
 #include "SmartGuesser.hpp"
 #include <stdlib.h>
+#include <iomanip>
 
-using std::string;
+using namespace std;
 using namespace bullpgia;
 
 string SmartGuesser::guess(){
@@ -10,8 +11,31 @@ string SmartGuesser::guess(){
 
 		}
 void SmartGuesser::learn (string st) {}
-void SmartGuesser::startNewGame(uint length) {}
+void SmartGuesser::startNewGame(uint length){
+	this->length = length;
+	buildset(length);
+}
 
+void SmartGuesser::buildset(int length){
+	string num;
+	int MAX_NUM;
+	int curr_length;
+	for(int i = 0; i < length; i++){
+		num += "9";
+	}
+	MAX_NUM = std::stoi(num);	
+	
+	for(int i=0; i <= MAX_NUM; i++){
+		string curr = to_string(i);
+		ostringstream ss;
+		ss  << setw(length) << setfill('0') << curr;
+		options.insert(ss.str());
+	}
+	for (set<string>::iterator it=options.begin(); it!=options.end(); ++it){
+		cout << ' ' << *it;
+		cout<<"\n";
+	}
+}
 
 
 
--- SmartGuesser.hpp
+++ SmartGuesser.hpp
@@ -3,11 +3,20 @@
 #include "Chooser.hpp"
 #include "calculate.hpp"
 #include <string>
-using std::string;
+#include <set>
+#include <iterator>
+
+using namespace std;
+
 namespace bullpgia{
+	
 class SmartGuesser: public bullpgia::Guesser {
-	string guess() override;
-	void learn (string st) override;
-	void startNewGame(uint length) override;
+	public:
+		string guess() override;
+		set<string> options = {};
+		void buildset(int length);
+		void remove_from_set(string status, set<string>);
+		void learn (string st) override;
+		void startNewGame(uint length) override;
 };
 }
