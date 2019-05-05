7893612096f0ad6c8936be9adfdd267b724069b0
adiweisberg <adi.weisberg5@gmail.com>
2019-05-05 12:34:50 +0300

    add notes

--- SmartGuesser.cpp
+++ SmartGuesser.cpp
@@ -6,16 +6,27 @@ using namespace std;
 using namespace bullpgia;
 
 string SmartGuesser::guess(){
-			string str;
-			return str;
+		string str;
+		#int currGuess = random(length);
+		#learn(calculateBullandPgia(currGuess), currGuess);
+		#check if there are 4 bulls - if so - win and return.
+		#else call learn.
+		return str;
 
 		}
-void SmartGuesser::learn (string st) {}
+void SmartGuesser::learn (string st) {
+	#need to call remove from here
+	#if 1 left - win
+	
+}
 void SmartGuesser::startNewGame(uint length){
 	this->length = length;
 	buildset(length);
 }
 
+int SmartGuesser::random(int length){
+	// rand between 0000 to length
+}
 void SmartGuesser::buildset(int length){
 	string num;
 	int MAX_NUM;
